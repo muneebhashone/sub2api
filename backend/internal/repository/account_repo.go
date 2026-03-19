05bc663eaf63eb4c04d89fbf4b2748b268d318d6
Hg <hging3@gmail.com>
2026-03-19 15:42:21 +0800

    feat: add ungrouped filter to account

--- backend/internal/repository/account_repo.go
+++ backend/internal/repository/account_repo.go
@@ -473,7 +473,9 @@ placeholder
 	if search != "" {
 		q = q.Where(dbaccount.NameContainsFold(search))
 placeholder
-	if groupID > 0 {
+	if groupID == service.AccountListGroupUngrouped {
+		q = q.Where(dbaccount.Not(dbaccount.HasAccountGroups()))
+placeholder else if groupID > 0 {
 		q = q.Where(dbaccount.HasAccountGroupsWith(dbaccountgroup.GroupIDEQ(groupID)))
 placeholder
 
