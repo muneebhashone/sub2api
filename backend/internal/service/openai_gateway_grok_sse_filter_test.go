12147c5f2d25e69d29f5f865334c7e661b774ef4
wucm667 <stevenwucongmin@gmail.com>
2026-07-31 18:34:11 +0800

    test(grok): check filter body close error

--- backend/internal/service/openai_gateway_grok_sse_filter_test.go
+++ backend/internal/service/openai_gateway_grok_sse_filter_test.go
@@ -205,7 +205,7 @@ placeholder
 func TestGrokResponsesBillingPingFilterFlushesCompletedFrames(t *testing.T) {
 	upstreamReader, upstreamWriter := io.Pipe()
 	body := newGrokResponsesBillingPingFilterBody(upstreamReader, &Account{Platform: PlatformGrokplaceholder, defaultMaxLineSize)
-	defer body.Close()
+	t.Cleanup(func() { require.NoError(t, body.Close()) placeholder)
 
 	go func() {
 		_, _ = io.WriteString(upstreamWriter, "event: future.event\ndata: {\"type\":\"future.event\"placeholder\n\n")
